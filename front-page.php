eeb11985523f9f3d7f73d6f0db9f0545e82de1f9
Jonathan <jacksaladin.js@gmail.com>
2026-05-12 19:58:19 +0200

    modification frontpage

--- front-page.php
+++ front-page.php
@@ -1,6 +1,41 @@
 <?php get_header(); ?>
 
 <main class="site-main">
+
+    <div class="home-intro">
+
+        <p class="home-intro-title">
+            Cet espace est fait pour vous simplifier la vie au possible :
+        </p>
+
+        <p class="home-line">Trouver une partie.</p>
+        <p class="home-line">Trouver des consommables.</p>
+        <p class="home-line">Trouver des répliques d'occasion ou neuves.</p>
+        <p class="home-line">Trouver un service de réparation ou de maintenance.</p>
+        <p class="home-line">Louer une réplique pour une date d'évènement.</p>
+        <p class="home-line">Revivre vos souvenirs grâce à la page Photos.</p>
+        <p class="home-line">Contacter des membres.</p>
+        <p class="home-line">Echanger sur divers sujets, via le forum.</p>
+        <p class="home-line">Suivre les réseaux sociaux de l'association.</p>
+
+        <p class="home-line">Et bien sûr, partager un maximum de moments funs et inoubliables !</p>
+
+        <p class="home-line">
+            Et pour finir, postuler, faire partie intégrante de l'association, et participer à sa vie active.
+        </p>
+
+        <p class="home-line">
+            Toute l'équipe vous souhaite une bonne navigation sur ce site, qui a été confectionné avec amour,
+            soin et implication. <span class="home-note">(Des qualités indispensables pour nous)</span>
+        </p>
+
+        <p class="home-final">
+            Longue vie à la <span class="wse-highlight">WSE</span> !
+        </p>
+
+    </div>
+
+
 </main>
 
 <!-- MODALE D'AGRANDISSEMENT D'IMAGE -->
