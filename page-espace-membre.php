89bfa5177532d0ccecf2e30eacdc6aa06d7276da
Jonathan <jacksaladin.js@gmail.com>
2026-05-15 17:55:06 +0200

    sécurisation de l'espace membre

--- page-espace-membre.php
+++ page-espace-membre.php
@@ -1,22 +1,20 @@
 <?php
 /* Template Name: Espace Membre */
 get_header();
+
+// Protection : accès réservé aux utilisateurs connectés
+if ( !is_user_logged_in() ) {
+    wp_redirect( wp_login_url() );
+    exit;
+}
 ?>
 
 <main class="site-main espace-membre">
-
-<?php if ( is_user_logged_in() ) : ?>
 
     <?php $user = wp_get_current_user(); ?>
     <p>Bienvenue, <?php echo esc_html( $user->display_name ); ?> !</p>
 
     <!-- Ici tu pourras afficher les infos wse_* plus tard -->
-
-<?php else : ?>
-
-    <p>Veuillez vous connecter pour accéder à l'espace membre.</p>
-
-<?php endif; ?>
 
 </main>
 
